efde580146b9e8de62cab62d36f7e40aa4a52cb6
Emir Güney Yavuzkaplan <103495760+emirguneyyavuzkaplan@users.noreply.github.com>
2023-01-07 00:29:41 +0300

    AdminProductComponent.php AdminCategoryComponent.php AdminEditCategoryComponent.php AdminAddCategoryComponent.php  Bittiler
    AdminProductComponent.php
    AdminCategoryComponent.php
    AdminEditCategoryComponent.php
    AdminAddCategoryComponent.php
    Bittiler

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminProductComponent;
 
 
 /*
@@ -58,9 +61,12 @@ Route::middleware(['auth:sanctum','verified'])->group(function (){
     Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
 });
 //FOR ADMİN
-Route::middleware(['auth:sanctum','verified','authadmin'])->group(function (){
-    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
-    Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function (){
+   Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+   Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
+   Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
+   Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
+   Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
 });
 
 
