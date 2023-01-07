405dff43ae7e87d8fd4fe15038c8e7201829b459
Emir Güney Yavuzkaplan <103495760+emirguneyyavuzkaplan@users.noreply.github.com>
2023-01-07 16:23:27 +0300

    güncell

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
+use App\Http\Livewire\Admin\AdminEditProductComponent;
 
 
 /*
@@ -67,6 +69,8 @@ Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])-
    Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
    Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
    Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
+   Route::get('admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
+   Route::get('/admin/product/edit/{product_slug}',AdminEditProductComponent::class)->name('admin.editproduct');
 });
 
 
