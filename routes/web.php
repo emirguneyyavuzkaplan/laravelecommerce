fee2e54c9c3aec02142cc8ad7f90cf35ec79e184
Emir Güney Yavuzkaplan <103495760+emirguneyyavuzkaplan@users.noreply.github.com>
2023-01-13 18:03:35 +0300

    Checkout Bitti
    CheckoutComponent.php
    ThankYouComponent.php
    Bittinler

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\WishlistComponent;
+use App\Http\Livewire\ThankYouComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
@@ -58,6 +59,8 @@ Route::get('/product-category/{category_slug}',CategoryComponent::class)->name('
 Route::get('/search',SearchComponent::class)->name('product.search');
 
 Route::get('/wishlist',WishlistComponent::class)->name('product.wishlist');
+
+Route::get('/thank-you',ThankYouComponent::class)->name('thankyou');
 
 
 //FOR USER  or Customer
